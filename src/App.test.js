b5f082aa41737022992937e0bf7f3de7a900769f
mrmgreen <m.r.green@hotmail.co.uk>
2019-04-03 06:35:01 +0100

    added enzyme and grid api test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import axios from 'axios';
+
+jest.mock('axios');
+const products = [
+  {
+    productId: 1391191,
+    title:"Indesit DIF 04B1 Ecotime Fully Integrated Dishwasher, White",
+    price:{  
+      was:"",
+      then1:"",
+      then2:"",
+      now:"220.00",
+      uom:"",
+      currency:"GBP"
+    },
+    image: "//johnlewis.scene7.com/is/image/JohnLewis/233326789?",
+  }
+];
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('makes a request to the product grid API when the grid page loads', () => {
+  const gridAPI = 'https://api.johnlewis.com/v1/products/search?q=dishwasher&key=Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb&pageSize=20';
+  axios.get.mockResolvedValue(products);
+  shallow(<App />);
+  expect(axios.get).toBeCalled();
+  expect(axios.get).toBeCalledWith(gridAPI);
 });
